7eb8f9e77abef83e79ea7cd25cf93a1de1968b89
Pallavi <pallavi3749@gmail.com>
2024-05-05 02:31:27 +0530

    fetching the jobs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
-
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchJobs } from './actions/jobAction';
+import Filter from "./components/Filter"
+import JobList from "./components/JobList"
 function App() {
+  const dispatch = useDispatch();
+  const { jobs} = useSelector(state => state.jobs);
+  useEffect(() => {
+    dispatch(fetchJobs());
+  }, [dispatch]);
+
+  console.log('jobs',jobs);
   return (
     <>
-      <h1> App</h1>
+      <Filter />
+      <JobList jobs={jobs} />
     </>
   )
 }
